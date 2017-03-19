bf6e6585f6b1140466bf807d3ffefb64010e3e23
Ivan-A-J-M <imelck@gmail.com>
2017-03-19 09:53:53 +0000

    Complete rewrite
    changed the whole subtraction system and and the interface

--- calculator.py
+++ calculator.py
@@ -1,14 +1,35 @@
-print("Welcome to the calculator,you can use the following operators:+,-,/,*,%" )
-x = input("Enter a number: ")
-a = raw_input("Operator: ")
-y = input("enter a second number: ")
-if a == "+":
-    print(x + y)
-elif a == "/":
-    print(x / y)
-elif a == "-":
-    print(x - y)
-elif a == "*":
-    print(x * y)
+print("Select operation.")
+print("1.addition")
+print("2.subtraction")
+print("3.multiplication")
+print("4.division")
+a = input("Enter choice (1/2/3/4):")
+
+
+
+
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+def add(x, y):
+        return x + y
+
+def devide(x, y):    
+        return x / y
+def subtract(x, y):     
+        return x - y
+    
+def multiply(x, y):   
+        return x * y
+if a == '1':
+    print(x,"+",y,"=", add(x, y))
+elif a == '2':
+    print(x,"-",y,"=", subtract(x, y))
+elif a == '3':
+    print(x,"*",y,"=", multiply(x, y))
+elif a == '4':
+    print(x,"/",y,"=", devide(x, y))
 else:
     print("Invalid operator")
+
